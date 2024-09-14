c65215de7ea47e652229e58fefd678b3aa10d47a
yuichi04 <crgone.910428.en@gmail.com>
2024-09-14 23:54:55 +0900

    DB接続処理を作成

---
+++ backend/db/db.go
@@ -0,0 +1,43 @@
+package db
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func NewDB() *gorm.DB {
+	if os.Getenv("GO_ENV") == "dev" {
+		err := godotenv.Load() // ローカルの.envの内容を読み込む
+		if err != nil {
+			log.Fatalln(err) // エラーメッセージをログに出力してからプログラムを強制終了する
+		}
+	}
+	// DB接続用のURLを生成
+	url := fmt.Sprintf("postgres://%s:%s@%s:%s/%s",
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PW"),
+		os.Getenv("POSTGRES_HOST"),
+		os.Getenv("POSTGRES_PORT"),
+		os.Getenv("POSTGRES_DB"),
+	)
+
+	// DBに接続
+	db, err := gorm.Open(postgres.Open(url), &gorm.Config{})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println("Successfully Connected")
+	return db
+}
+
+func CloseDB(db *gorm.DB) {
+	sqlDB, _ := db.DB()
+	if err := sqlDB.Close(); err != nil {
+		log.Fatalln(err)
+	}
+}
---
+++ backend/migrate/migrate.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"backend/db"
+	"backend/model"
+	"fmt"
+)
+
+func main() {
+	dbConn := db.NewDB()
+	defer fmt.Println("Successfully Migrated")
+	defer db.CloseDB(dbConn)
+	dbConn.AutoMigrate(&model.User{}, &model.Task{})
+}
