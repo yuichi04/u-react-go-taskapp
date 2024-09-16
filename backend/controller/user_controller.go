30d1b10beb05a222e62f82561a282ee2c44c2226
yuichi04 <crgone.910428.en@gmail.com>
2024-09-17 00:16:06 +0900

    user controllerを作成

---
+++ backend/controller/user_controller.go
@@ -0,0 +1,86 @@
+/*
+Controllerの責務
+HTTPリクエストの受け取りとレスポンスの返却を担当
+リクエストから必要なデータを抽出し、それをユースケース層に渡して処理を依頼し、
+結果をクライアントサイドに返す。基本的には入力のバリデーション、ユースケースの呼び出し、レスポンスの生成を行う
+*/
+
+package controller
+
+import (
+	"backend/model"
+	"backend/usecase"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/labstack/echo/v4"
+)
+
+type IUserController interface {
+	SignUp(c echo.Context) error
+	LogIn(c echo.Context) error
+	LogOut(c echo.Context) error
+	CsrfToken(c echo.Context) error
+}
+
+type userController struct {
+	uu usecase.IUserUsecase
+}
+
+func NewUserController(uu usecase.IUserUsecase) IUserController {
+	return &userController{uu}
+}
+
+func (uc *userController) SignUp(c echo.Context) error {
+	user := model.User{}
+	if err := c.Bind(&user); err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+	userRes, err := uc.uu.SignUp(user)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+	return c.JSON(http.StatusCreated, userRes)
+}
+
+func (uc *userController) LogIn(c echo.Context) error {
+	user := model.User{}
+	if err := c.Bind(&user); err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+	tokenString, err := uc.uu.LogIn(user)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+	cookie := new(http.Cookie)
+	cookie.Name = "token"
+	cookie.Value = tokenString
+	cookie.Expires = time.Now().Add(24 * time.Hour)
+	cookie.Path = "/"
+	cookie.Domain = os.Getenv("API_DOMAIN")
+	cookie.HttpOnly = true
+	cookie.SameSite = http.SameSiteNoneMode
+	c.SetCookie(cookie)
+	return c.NoContent(http.StatusOK)
+}
+
+func (uc *userController) LogOut(c echo.Context) error {
+	cookie := new(http.Cookie)
+	cookie.Name = "token"
+	cookie.Value = ""
+	cookie.Expires = time.Now()
+	cookie.Path = "/"
+	cookie.Domain = os.Getenv("API_DOMAIN")
+	cookie.HttpOnly = true
+	cookie.SameSite = http.SameSiteNoneMode
+	c.SetCookie(cookie)
+	return c.NoContent(http.StatusOK)
+}
+
+func (uc *userController) CsrfToken(c echo.Context) error {
+	token := c.Get("csrf").(string)
+	return c.JSON(http.StatusOK, echo.Map{
+		"csrf_token": token,
+	})
+}
--- backend/repository/user_repository.go
+++ backend/repository/user_repository.go
@@ -1,8 +1,39 @@
+/*
+Repositoryの責務
+データソース（データベースや外部APIなど）とのやりとりを担当
+データの永続化や検索など、データに関する操作を抽象化し、Usecase層からデータソースの詳細を隠蔽する
+*/
 package repository
 
-import "backend/model"
+import (
+	"backend/model"
+
+	"gorm.io/gorm"
+)
 
 type IUserRepository interface {
 	GetUserByEmail(user *model.User, email string) error
 	CreateUser(user *model.User) error
+}
+
+type userRepository struct {
+	db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) IUserRepository {
+	return &userRepository{db}
+}
+
+func (ur *userRepository) GetUserByEmail(user *model.User, email string) error {
+	if err := ur.db.Where("email=?", email).First(user).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (ur *userRepository) CreateUser(user *model.User) error {
+	if err := ur.db.Create(user).Error; err != nil {
+		return err
+	}
+	return nil
 }
